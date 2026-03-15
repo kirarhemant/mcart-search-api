a4eaaaa3dae781b93748a266b8d234622012b400
Hemant Kirar <hemant.kirar+aswatson@nagarro.com>
2026-03-15 15:24:42 +0530

    auth via spring security added

---
+++ src/main/java/com/mcart/search/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.mcart.search.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health", "/actuator/info")
+                        .permitAll()
+                        .requestMatchers("/api/**")
+                        .authenticated()
+                        .anyRequest()
+                        .permitAll())
+                .oauth2ResourceServer(oauth -> oauth.jwt()); // JWT Bearer tokens
+        return http.build();
+    }
+}
